187503275210bdf6dbc30049d3c92b3915ed4371
keely glover <Keely.kay.g@gmail.com>
2024-08-06 15:15:14 -0400

    patient profile

---
+++ vue/src/components/AppointmentsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <table>
+          <thead>
+              <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Reason</th>
+                  <th>Appointment Date</th>
+                  <th>Appointment Time</th>
+                  <th>Doctor Id</th>
+                  <th>Appointment Id</th>
+              </tr>
+          </thead>
+
+        <tbody>
+            <tr v-for="appointments in appointmentsList" v-bind:key="appointments.appointmentId">
+                <td>{{ appointments.first }}</td>
+                <td>{{ appointments.last }}</td>
+                <td>{{ appointments.reason }}</td>
+                <td>{{ appointments.apptDate }}</td>
+                <td>{{ appointments.doctor }}</td>
+                <td>{{ appointments.apptId }}</td>
+
+            </tr>
+        </tbody>
+
+      </table>
+</template>
+
+<script>
+export default {
+    props: ['appointmentsList']
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/components/BookAppointment.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>this is where you would book an appointment</div>
+  <form>
+
+<div class="inputForm">
+  <label for="firstName">What is your first name? </label>
+  <input id="firstName" type="text" v-model="firstName" />
+</div>
+
+<div class="inputForm">
+  <label for="lastName">What is your last name? </label>
+  <input id="lastName" type="text" v-model="lastName" />
+</div>
+
+<div class="inputForm">
+  <label>What is the reason for your appointment? </label>
+  <select v-model="reason">
+    <option value="checkUp">Check Up</option>
+    <option value="physical">Physical</option>
+    <option value="urgent">Urgent</option>
+    <option value="coldFlu">Cold/Flu</option>
+    <option value="other">Other</option>
+  </select>
+</div>
+
+<div class="inputForm">
+  <label for="appointmentDate">Which day would you like? </label>
+  <input id="appointmentDate" type="date" v-model="appointmentDate" />
+</div>
+
+
+<!-- add actual doctor times instead of this -->
+<div class="inputForm">
+  <label>What time would work best for you? </label>
+  <select v-model="appointmentTime">
+    <option value="am">Morning</option>
+    <option value="pm">Evening</option>
+    <option value="afternoon">Afternoon</option>
+  </select>
+</div>
+
+<div class="inputForm">
+  <label for="doctorId">What is your doctors Id? </label>
+  <input id="doctorId" type="text" v-model="doctorId" />
+</div>
+
+
+<input type="submit" value="Book it!" @click.prevent="handleSubmit" />
+
+</form>
+</template>
+
+<script>
+
+import AppointmentService from '../services/AppointmentService.js';
+
+export default {
+  data() {
+    return {
+      appointment: {
+     
+      firstName: '',
+      lastName: '',
+      reason: '',
+      appointmentDate: '',
+      appointmentTime: '',
+      doctorId: '',
+      appointmentId: '',
+      }
+      
+    };
+  },
+
+  methods: {
+    appointmentForm() {
+
+      const newApptObject = 
+      {
+        first: this.firstName,
+        last: this.lastName,
+        reason: this.reason,
+        apptDate: this.appointmentDate,
+        apptTime: this.appointmentTime,
+        doctor: this.doctorId,
+        apptId: this.appointmentId
+      };
+
+      AppointmentService.createAppointment(newApptObject).then((r) => { 
+        console.log(r);
+        this.$router.push({ name: "patientView" });
+      }).catch((e) => { console.error(e)});
+
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
