c8b102d10e49d594002824a0ebd2677c2bdf70e2
keely glover <Keely.kay.g@gmail.com>
2024-08-07 12:11:45 -0400

    keely doing things

--- vue/src/services/PatientService.js
+++ vue/src/services/PatientService.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export default {
-        createPatient(patientObject) {
-            return axios.post("/patient/register", patientObject)
-        }
+        // createPatient(patientObject) {
+        //     return axios.post("/patient/register", patientObject)
+        // },
+
+        // getPatient(){
+        //     return axios.get("/patient")
+        // }
+
 }
