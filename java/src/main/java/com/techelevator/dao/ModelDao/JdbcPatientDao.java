6dac3de7e4d9ad73ff9f89e01af5485b3a4fa2d4
Ding Jiang <dingj106@gmail.com>
2024-08-07 11:02:50 -0400

    merge

--- java/src/main/java/com/techelevator/dao/ModelDao/JdbcPatientDao.java
+++ java/src/main/java/com/techelevator/dao/ModelDao/JdbcPatientDao.java
@@ -1,6 +1,14 @@
 package com.techelevator.dao.ModelDao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcPatientDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
 
 
