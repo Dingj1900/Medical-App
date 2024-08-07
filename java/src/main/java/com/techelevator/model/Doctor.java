5f6bb215021800dd24dfa76de6098d81afe369f6
Heather Gee <hzhang27hz@gmail.com>
2024-08-07 14:43:57 -0400

    login attempt

--- java/src/main/java/com/techelevator/model/Doctor.java
+++ java/src/main/java/com/techelevator/model/Doctor.java
@@ -1,168 +1,168 @@
-//package com.techelevator.model;
-//
-//import java.time.LocalTime;
-//
-//public class Doctor {
-//
-//    private int doctorId;
-//    private String firstName;
-//    private String lastName;
-//    private String gender;
-//    private String phoneNumber;
-//    private String email;
-//
-//    private LocalTime hoursFrom;
-//    private LocalTime hoursTo;
-//
-//    private boolean isMonday;
-//    private boolean isTuesday;
-//    private boolean isWednesday;
-//    private boolean isThursday;
-//    private boolean isFriday;
-//    private boolean isSaturday;
-//    private boolean isSunday;
-//
-//    public Doctor() {
-//
-//    }
-//
-//    public Doctor(int doctorId, String firstName, String lastName, String gender, String phoneNumber, String email,
-//                  LocalTime hoursFrom, LocalTime hoursTo, boolean isMonday, boolean isTuesday, boolean isWednesday, boolean isThursday, boolean isFriday, boolean isSaturday, boolean isSunday) {
-//        this.doctorId = doctorId;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.gender = gender;
-//        this.phoneNumber = phoneNumber;
-//        this.email = email;
-//        this.hoursFrom = hoursFrom;
-//        this.hoursTo = hoursTo;
-//        this.isMonday = isMonday;
-//        this.isTuesday = isTuesday;
-//        this.isWednesday = isWednesday;
-//        this.isThursday = isThursday;
-//        this.isFriday = isFriday;
-//        this.isSaturday = isSaturday;
-//        this.isSunday = isSunday;
-//    }
-//
-//    public int getDoctorId() {
-//        return doctorId;
-//    }
-//
-//    public void setDoctorId(int doctorId) {
-//        this.doctorId = doctorId;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getGender() {
-//        return gender;
-//    }
-//
-//    public void setGender(String gender) {
-//        this.gender = gender;
-//    }
-//
-//    public String getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public void setPhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//
-//    public LocalTime getHoursFrom() {
-//        return hoursFrom;
-//    }
-//
-//    public void setHoursFrom(LocalTime hoursFrom) {
-//        this.hoursFrom = hoursFrom;
-//    }
-//
-//    public LocalTime getHoursTo() {
-//        return hoursTo;
-//    }
-//
-//    public void setHoursTo(LocalTime hoursTo) {
-//        this.hoursTo = hoursTo;
-//    }
-//
-//    public boolean isMonday() {
-//        return isMonday;
-//    }
-//
-//    public void setMonday(boolean monday) {
-//        isMonday = monday;
-//    }
-//
-//    public boolean isTuesday() {
-//        return isTuesday;
-//    }
-//
-//    public void setTuesday(boolean tuesday) {
-//        isTuesday = tuesday;
-//    }
-//
-//    public boolean isWednesday() {
-//        return isWednesday;
-//    }
-//
-//    public void setWednesday(boolean wednesday) {
-//        isWednesday = wednesday;
-//    }
-//
-//    public boolean isThursday() {
-//        return isThursday;
-//    }
-//
-//    public void setThursday(boolean thursday) {
-//        isThursday = thursday;
-//    }
-//
-//    public boolean isFriday() {
-//        return isFriday;
-//    }
-//
-//    public void setFriday(boolean friday) {
-//        isFriday = friday;
-//    }
-//
-//    public boolean isSaturday() {
-//        return isSaturday;
-//    }
-//
-//    public void setSaturday(boolean saturday) {
-//        isSaturday = saturday;
-//    }
-//
-//    public boolean isSunday() {
-//        return isSunday;
-//    }
-//
-//    public void setSunday(boolean sunday) {
-//        isSunday = sunday;
-//    }
-//}
+package com.techelevator.model;
+
+import java.time.LocalTime;
+
+public class Doctor {
+
+    private int doctorId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+
+    private LocalTime hoursFrom;
+    private LocalTime hoursTo;
+
+    private boolean isMonday;
+    private boolean isTuesday;
+    private boolean isWednesday;
+    private boolean isThursday;
+    private boolean isFriday;
+    private boolean isSaturday;
+    private boolean isSunday;
+
+    public Doctor() {
+
+    }
+
+    public Doctor(int doctorId, String firstName, String lastName, String gender, String phoneNumber, String email,
+                  LocalTime hoursFrom, LocalTime hoursTo, boolean isMonday, boolean isTuesday, boolean isWednesday, boolean isThursday, boolean isFriday, boolean isSaturday, boolean isSunday) {
+        this.doctorId = doctorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.hoursFrom = hoursFrom;
+        this.hoursTo = hoursTo;
+        this.isMonday = isMonday;
+        this.isTuesday = isTuesday;
+        this.isWednesday = isWednesday;
+        this.isThursday = isThursday;
+        this.isFriday = isFriday;
+        this.isSaturday = isSaturday;
+        this.isSunday = isSunday;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public LocalTime getHoursFrom() {
+        return hoursFrom;
+    }
+
+    public void setHoursFrom(LocalTime hoursFrom) {
+        this.hoursFrom = hoursFrom;
+    }
+
+    public LocalTime getHoursTo() {
+        return hoursTo;
+    }
+
+    public void setHoursTo(LocalTime hoursTo) {
+        this.hoursTo = hoursTo;
+    }
+
+    public boolean isMonday() {
+        return isMonday;
+    }
+
+    public void setMonday(boolean monday) {
+        isMonday = monday;
+    }
+
+    public boolean isTuesday() {
+        return isTuesday;
+    }
+
+    public void setTuesday(boolean tuesday) {
+        isTuesday = tuesday;
+    }
+
+    public boolean isWednesday() {
+        return isWednesday;
+    }
+
+    public void setWednesday(boolean wednesday) {
+        isWednesday = wednesday;
+    }
+
+    public boolean isThursday() {
+        return isThursday;
+    }
+
+    public void setThursday(boolean thursday) {
+        isThursday = thursday;
+    }
+
+    public boolean isFriday() {
+        return isFriday;
+    }
+
+    public void setFriday(boolean friday) {
+        isFriday = friday;
+    }
+
+    public boolean isSaturday() {
+        return isSaturday;
+    }
+
+    public void setSaturday(boolean saturday) {
+        isSaturday = saturday;
+    }
+
+    public boolean isSunday() {
+        return isSunday;
+    }
+
+    public void setSunday(boolean sunday) {
+        isSunday = sunday;
+    }
+}
