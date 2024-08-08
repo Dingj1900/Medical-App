32ce1c330fb698dc181f9fc73c427eacbb645990
Heather Gee <hzhang27hz@gmail.com>
2024-08-08 17:21:29 -0400

    office

--- vue/src/components/Office.vue
+++ vue/src/components/Office.vue
@@ -1,31 +1,37 @@
 <template>
     <div>
         <div :style="cssProps"></div>
-        <h3 class="officeName"></h3>
-        <h3 class="address"></h3>
-        <h3 class="phoneNumber"></h3>
-        <h3 class="monday"></h3>
-        <h3 class="tuesday"></h3>
-        <h3 class="wednesday"></h3>
-        <h3 class="thursday"></h3>
-        <h3 class="friday"></h3>
-        <h3 class="saturday"></h3>
-        <h3 class="sunday"></h3>
-        <h3 class="doctors"></h3>
-        <h3 class="costPerHour"></h3>
+        <div class="office info container">
+        <h2>Office Name:</h2>
+        <h3 class="officeName">{{ office.officeName }}</h3>
+        <h2>Address:</h2>
+        <h3 class="address">{{ office.address }}</h3>
+        <h2>Phone Number:</h2>
+        <h3 class="phoneNumber">{{ office.phoneNumber }}</h3>
+        <h2>Office Hours:</h2>
+        <h3 class="monday">{{ office.isMonday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="tuesday">{{ office.isTuesday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="wednesday">{{ office.isWednesday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="thursday">{{ office.isThursday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="friday">{{ office.isFriday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="saturday">{{ office.isSaturday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h3 class="sunday">{{ office.isSunday }}{{ office.hoursFrom }}{{ office.hoursTo }}</h3>
+        <h2>Doctors:</h2>
+        <h3 class="doctors">Dr. {{ user.last_name }}</h3>
+        <h2>Service Cost:</h2>
+        <h3 class="costPerHour">{{ services.hourlyRate }}</h3>
+        </div>
     </div>
 </template>
 
 <script>
  export default {
-    data() {
-      return {
+    return: {
         cssProps: {
           backgroundImage: `url(${require('@/assets/officebase1img.jpg')})`
         }
-      }
+      },
     }
-  }
 </script>
 
 <style scoped>
