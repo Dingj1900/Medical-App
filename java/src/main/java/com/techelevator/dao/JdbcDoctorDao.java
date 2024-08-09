16a8df4fe403d0c3ecbe5b25a66b237e4337b375
Heather Gee <hzhang27hz@gmail.com>
2024-08-09 14:10:35 -0400

    doctor jbdc

--- java/src/main/java/com/techelevator/dao/JdbcDoctorDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcDoctorDao.java
@@ -7,10 +7,11 @@ import com.techelevator.model.User;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class JdbcDoctorDao implements DoctorDao {
 
     private final JdbcTemplate jdbcTemplate;
@@ -21,9 +22,19 @@ public class JdbcDoctorDao implements DoctorDao {
 
     public List<Appointment> getAppointmentsByDoctor(int doctorId) {
         List<Appointment> appointments = new ArrayList<>();
-        String sql =
-
-    }
+        String sql = "SELECT * " +
+                "FROM appointment " + "WHERE doctor_id = ? ";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, doctorId);
+            while (results.next()) {
+                Appointment appointment = mapRowToAppointment(results);
+                appointments.add(appointment);
+            }
+            } catch(CannotGetJdbcConnectionException e){
+                throw new DaoException("Unable to connect to server or database", e);
+            }
+            return appointments;
+        }
 
     public Office getOfficeByDoctor(int doctorId) {
         Office office = null;
@@ -64,4 +75,40 @@ public class JdbcDoctorDao implements DoctorDao {
     }
 
 
+    private Appointment mapRowToAppointment(SqlRowSet rs){
+        Appointment appointment = new Appointment();
+        appointment.setAppointmentId(rs.getInt("appointment_id"));
+        appointment.setOfficeId(rs.getInt("office_id"));
+        appointment.setPatientId(rs.getInt("patient_id"));
+        appointment.setDoctorId(rs.getInt("doctor_id"));
+        try {
+            if(rs.getDate("appt_from") != null) {
+                appointment.setApptFrom(rs.getTime("appt_from").toLocalTime());;
+            }
+            if(rs.getDate("hours_to") != null){
+                appointment.setApptTo(rs.getTime("hours_to").toLocalTime());
+            }
+
+            appointment.setMonday((boolean) rs.getObject("is_monday"));
+            appointment.setTuesday((boolean) rs.getObject("is_tuesday"));
+            appointment.setWednesday((boolean) rs.getObject("is_wednesday"));
+            appointment.setThursday((boolean) rs.getObject("is_thursday"));
+            appointment.setFriday((boolean) rs.getObject("is_friday"));
+            appointment.setSaturday((boolean) rs.getObject("is_saturday"));
+            appointment.setSunday((boolean) rs.getObject("is_sunday"));
+            appointment.setNotified((boolean)rs.getObject("is_notified"));
+            appointment.setApproved((boolean)rs.getObject("is_approved"));
+        } catch (NullPointerException error){
+        throw new DaoException("Null pointer exception for a user value", error);
+        }catch(Exception error){
+        throw new DaoException("general mapper error", error);
+         }
+
+        return appointment;
 }
+
+
+
+    }
+
+
--- java/src/main/java/com/techelevator/model/Appointment.java
+++ java/src/main/java/com/techelevator/model/Appointment.java
@@ -1,20 +1,41 @@
 package com.techelevator.model;
 
+import java.time.LocalTime;
+
 public class Appointment {
 
     private int appointmentId;
     private int officeId;
     private int patientId;
     private  int doctorId;
+    private LocalTime apptFrom;
+    private LocalTime apptTo;
+    private boolean isMonday;
+    private boolean isTuesday;
+    private boolean isWednesday;
+    private boolean isThursday;
+    private boolean isFriday;
+    private boolean isSaturday;
+    private boolean isSunday;
     private boolean isNotified;
     private boolean isApproved;
 
+    public Appointment () {}
 
     public Appointment(int appointmentId, int officeId, int patientId, int doctorId, boolean isNotified, boolean isApproved) {
         this.appointmentId = appointmentId;
         this.officeId = officeId;
         this.patientId = patientId;
         this.doctorId = doctorId;
+        this.apptFrom = apptFrom;
+        this.apptTo = apptTo;
+        this.isMonday = isMonday;
+        this.isTuesday = isTuesday;
+        this.isWednesday = isWednesday;
+        this.isThursday = isThursday;
+        this.isFriday = isFriday;
+        this.isSaturday = isSaturday;
+        this.isSunday = isSunday;
         this.isNotified = isNotified;
         this.isApproved = isApproved;
     }
@@ -70,6 +91,78 @@ public class Appointment {
 
     public void setApproved(boolean approved) {
         isApproved = approved;
+    }
+
+    public LocalTime getApptFrom() {
+        return apptFrom;
+    }
+
+    public void setApptFrom(LocalTime apptFrom) {
+        this.apptFrom = apptFrom;
+    }
+
+    public LocalTime getApptTo() {
+        return apptTo;
+    }
+
+    public void setApptTo(LocalTime apptTo) {
+        this.apptTo = apptTo;
+    }
+
+    public boolean isMonday() {
+        return isMonday;
+    }
+
+    public void setMonday(boolean monday) {
+        isMonday = monday;
+    }
+
+    public boolean isTuesday() {
+        return isTuesday;
+    }
+
+    public void setTuesday(boolean tuesday) {
+        isTuesday = tuesday;
+    }
+
+    public boolean isWednesday() {
+        return isWednesday;
+    }
+
+    public void setWednesday(boolean wednesday) {
+        isWednesday = wednesday;
+    }
+
+    public boolean isThursday() {
+        return isThursday;
+    }
+
+    public void setThursday(boolean thursday) {
+        isThursday = thursday;
+    }
+
+    public boolean isFriday() {
+        return isFriday;
+    }
+
+    public void setFriday(boolean friday) {
+        isFriday = friday;
+    }
+
+    public boolean isSaturday() {
+        return isSaturday;
+    }
+
+    public void setSaturday(boolean saturday) {
+        isSaturday = saturday;
+    }
+
+    public boolean isSunday() {
+        return isSunday;
+    }
+
+    public void setSunday(boolean sunday) {
+        isSunday = sunday;
     }
 }
 
