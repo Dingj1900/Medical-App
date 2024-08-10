e109f4bef9dfd531e3ab7c1f01e634c23d77a1db
Rashid Sowah <sowahrashid@yahoo.com>
2024-08-10 13:17:48 -0400

    DocDao

--- java/src/main/java/com/techelevator/dao/DoctorDao.java
+++ java/src/main/java/com/techelevator/dao/DoctorDao.java
@@ -1,6 +1,7 @@
 package com.techelevator.dao;
 
 
+import com.techelevator.model.Appointment;
 import com.techelevator.model.Office;
 import com.techelevator.model.RegisterUserDto;
 import com.techelevator.model.User;
@@ -12,5 +13,12 @@ public interface DoctorDao {
 
     Office getOfficeByDoctor(int doctorId);
 
+    Appointment getAppointmentByDoctor(int doctorId);
+
+    List<Appointment> getAppointmentsByDoctor(int doctorId);
+
+    Office updateOfficeById(Office office);
+
+    int createOffice(Office office, int doctorId);
 
 }
