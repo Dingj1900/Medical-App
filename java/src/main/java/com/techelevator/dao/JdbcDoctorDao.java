fb7f3f0cfffe533b83474d45de64f9c88faa9857
Heather Gee <hzhang27hz@gmail.com>
2024-08-10 14:24:11 -0400

    update

--- java/src/main/java/com/techelevator/dao/JdbcDoctorDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcDoctorDao.java
@@ -3,7 +3,7 @@ package com.techelevator.dao;
 import com.techelevator.exception.DaoException;
 import com.techelevator.model.Appointment;
 import com.techelevator.model.Office;
-import com.techelevator.model.User;
+import com.techelevator.model.Services;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -133,6 +133,95 @@ public class JdbcDoctorDao implements DoctorDao {
         return updatedOffice;
     }
 
+    public Services getServiceByDoctor(int doctorId) {
+        Services service = null;
+
+        String sql = "SELECT * " +
+                "FROM services " +
+                "WHERE doctor_id = ? ";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, doctorId);
+            if (results.next()) {
+                service = mapRowToServices(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return service;
+    }
+
+    public List<Services> getServicesByDoctor(int doctorId){
+        List<Services> services = new ArrayList<>();
+
+        String sql = "SELECT * " +
+                "FROM services " + "WHERE doctor_id = ? ";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, doctorId);
+            while (results.next()) {
+                Services service = mapRowToServices(results);
+                services.add(service);
+            }
+        } catch(CannotGetJdbcConnectionException e){
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return services;
+    }
+
+    public Services updateServiceById(Services services){
+
+        Services updatedServices = null;
+        String sql = "UPDATE services SET service_name = ?, service_details = ?, hourly_rate = ?, " +
+                "doctor_id = ? " +
+                "WHERE service_id = ?";
+
+        try {
+            int numberOfRows = jdbcTemplate.update(sql, services.getServiceName(), services.getServiceDetails(),
+                    services.getHourlyRate(), services.getDoctorId(), services.getServiceId());
+
+            if (numberOfRows == 0) {
+                throw new DaoException("Zero rows affected, expected at least one");
+            } else {
+                updatedServices = getServiceByDoctor(services.getServiceId());
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return updatedServices;
+    }
+
+    public int createService(Services services, int doctorId){
+        int newServiceId = 0;
+
+        String serviceName = services.getServiceName();
+        String serviceDetails = services.getServiceDetails();
+        Double hourlyRate = services.getHourlyRate();
+        doctorId = services.getDoctorId();
+
+
+        String sql = "INSERT INTO services " +
+                "(service_name, service_details, hourly_rate, doctor_id) " +
+                "values (LOWER(TRIM(?)), ?, ?, ?, ?) RETURNING service_id";
+
+        try {
+            newServiceId = jdbcTemplate.queryForObject
+                    (sql, int.class, serviceName, serviceDetails, hourlyRate, doctorId);
+
+            getServiceByDoctor(newServiceId);
+
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }catch(NullPointerException error){
+            throw new DaoException("Unable to process user data, Null pointer exception", error);
+        }
+        return newServiceId;
+
+    }
+
     private Office mapRowToOffice(SqlRowSet rs) {
         Office office = new Office();
         office.setOfficeId(rs.getInt("office_id"));
@@ -187,7 +276,16 @@ public class JdbcDoctorDao implements DoctorDao {
         return appointment;
 }
 
+    private Services mapRowToServices(SqlRowSet rs){
+        Services services = new Services();
+        services.setServiceId(rs.getInt("service_id"));
+        services.setServiceName(rs.getString("service_name"));
+        services.setServiceDetails(rs.getString("service_details"));
+        services.setHourlyRate(rs.getDouble("hourly_rate"));
+        services.setDoctorId(rs.getInt("doctor_id"));
 
+        return services;
+    }
 
     }
 
--- java/src/main/java/com/techelevator/dao/ModelDao/ServicesDao.java
+++
@@ -1,4 +0,0 @@
-package com.techelevator.dao.ModelDao;
-
-public interface ServicesDao {
-}
