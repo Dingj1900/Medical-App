834af6cdd123fc68577aa96a07b0a532970d2c8e
Heather Gee <hzhang27hz@gmail.com>
2024-08-11 16:14:04 -0400

    appointment

---
+++ vue/src/components/PatientAppointmentList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="appointment" v-for="appointment in appointments" v-bind:key="appointment.id">
+      <router-link v-bind:to="{ name: 'TopicDetailsView', params: { topicId: topic.id } }">
+        {{ topic.title }}  
+      </router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      appointments: {
+        type: Array,
+        required: true
+      }
+    },
+  };
+  </script>
+  
