e83a7a391f62cc956855330fcb5c1180e9aac87e
Ding Jiang <dingj106@gmail.com>
2024-08-11 16:12:59 -0400

    turn off has role for postman and vue

--- java/src/main/java/com/techelevator/controller/PatientController.java
+++ java/src/main/java/com/techelevator/controller/PatientController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @CrossOrigin
 
-@PreAuthorize("hasRole('Patient')") // rash
+//@PreAuthorize("hasRole('Patient')") // rash
 
 public class PatientController {
 
