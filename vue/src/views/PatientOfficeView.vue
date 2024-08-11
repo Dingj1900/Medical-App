f8c7cdb572296e9bbe822fcb2ef07f35e3d4d41d
Heather Gee <hzhang27hz@gmail.com>
2024-08-11 10:55:42 -0400

    patient office

---
+++ vue/src/views/PatientOfficeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <patientOffice />
+    </div>
+</template>
+
+<script>
+import patientOffice from '../components/PatientOffice.vue';
+
+export default {
+    components: {
+        patientOffice
+    }
+}
+
+</script>
+
+<style scoped>
+</style>
