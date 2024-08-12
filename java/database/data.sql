15cba511ff8ee42074e9b0e6f3c33504ea08236d
Sheena Ortiz <sheena.ortiz@gmail.com>
2024-08-12 18:09:10 -0400

    Fixed data.sql

--- java/database/data.sql
+++ java/database/data.sql
@@ -19,18 +19,18 @@ VALUES (1, 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC
 
 INSERT INTO office(office_id, office_name, office_address, phone_number, hours_from, hours_to)
 VALUES(111 ,'keelys office','111 Kinnear road','614','10:00:00','18:00:00'),
-(12 ,'COPC','111 Park Ave','614-980-3842','08:00:00','16:00:00'),
-(111 ,'Ohio State Medical','111 Kenny road','419-801-3824','09:00:00','17:00:00');
+      (12  ,'COPC','111 Park Ave','614-980-3842','08:00:00','16:00:00'),
+      (112 ,'Ohio State Medical','111 Kenny road','419-801-3824','09:00:00','17:00:00');
 
 INSERT INTO services(service_name, service_details, hourly_rate, doctor_id)
-VALUES ( 'help', 'my arm literally fell off', 1.2, 12),
-       ( 'Check up', 'Need a COVID shot', 100, 10),
-       ( 'Well Visit', 'School physical', 160, 02);
+VALUES ( 'help', 'my arm literally fell off', 1.2, 113),
+       ( 'Check up', 'Need a COVID shot', 100, 113),
+       ( 'Well Visit', 'School physical', 160, 113);
 
 INSERT INTO appointment(appointment_id, service_id, office_id, patient_id, doctor_id, appt_date, is_notified, is_approved)
-VALUES (121, 2, 111, 1, 12,'20240922 09:00:00', 'true', 'true'),
-(121, 4, 110, 1, 12,'20240922 09:00:00', 'false', 'false'),
-(131, 8, 90, 1, 12,'20240922 09:00:00', 'true', 'true');
+VALUES (121, 1, 111, 1, 12,'20240922 09:00:00', 'true', 'true'),
+       (124, 2, 12,  1, 12,'20240922 09:00:00', 'false', 'false'),
+       (131, 3, 112, 1, 12,'20240922 09:00:00', 'true', 'true');
 
 
 
