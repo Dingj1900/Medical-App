2e98717dcd16e9910ad01901c0655e6fc637cd1e
Ding Jiang <dingj106@gmail.com>
2024-08-13 10:17:42 -0400

    merge

--- vue/src/views/PatientView.vue
+++ vue/src/views/PatientView.vue
@@ -127,11 +127,9 @@
     <div class="w3-col m2">
       <div class="w3-card w3-round w3-white w3-center">
         <div class="w3-container">
-          <p>Upcoming Appointments:</p>
-          <img src="/w3images/forest.jpg" alt="Forest" style="width:100%;">
-          <p><strong>Holiday</strong></p>
-          <p>Friday 15:00</p>
-          <p><button class="w3-button w3-block w3-theme-l4">Info</button></p>
+          <p><strong>View offices to make an appointment:</strong></p>
+
+          <p><button class="w3-button w3-block w3-theme-l4">Make Appointment</button></p>
         </div>
       </div>
       <br>
